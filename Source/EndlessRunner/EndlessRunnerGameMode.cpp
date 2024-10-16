b90eab4d4e32cd210dd51afac39b9988386cbdf2
SirKentut <pierreharbin@gmail.com>
2024-10-16 21:22:47 +0900

    initializing unreal engine

---
+++ Source/EndlessRunner/EndlessRunnerGameMode.cpp
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EndlessRunnerGameMode.h"
+#include "EndlessRunnerCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AEndlessRunnerGameMode::AEndlessRunnerGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+}
